48e690c3d50e75a0e12f6e41f45c248e3d3f6fd7
erziealdrian02 <erzie.aldrian02@gmail.com>
2024-12-12 10:12:54 +0700

    Add more

--- webpack.common.js
+++ webpack.common.js
@@ -38,6 +38,10 @@ module.exports = {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
         },
+        {
+          from: path.resolve(__dirname, '.nojekyll'),
+          to: path.resolve(__dirname, 'dist/.nojekyll'),
+        },
       ],
     }),
   ],
