f70e3caa1b92e1281abf0af9c335aa4824734382
erziealdrian02 <erzie.aldrian02@gmail.com>
2025-05-17 16:04:09 +0700

    For Deploye ment

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // ⛔️ Disable ESLint during build
   },
+  output: 'export', // Export to static HTML files
+  distDir: 'out', // Output to the 'out' directory
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
